a3dcbff8701de225ddad7c33c7b508a76e030ee4
iadamczyk <78596575+iadamczyk@users.noreply.github.com>
2021-02-05 12:52:58 +0100

    Update script.js

---
+++ 1.Początki/script.js
@@ -0,0 +1,12 @@
+//1 
+console.log('Iza');
+
+//2
+console.log(12+32);
+console.log(52/16);
+console.log(5*2);
+
+//3
+console.log("Hello World");
+console.log(Hello World);  //blad
+console.log("Hello World); //blad
