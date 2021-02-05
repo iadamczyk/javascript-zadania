12eac58303a3b91c2cae371abc574f59ff0f7c46
iadamczyk <78596575+iadamczyk@users.noreply.github.com>
2021-02-05 12:55:49 +0100

    Update script.js

---
+++ 3.Operatory/script.js
@@ -0,0 +1,40 @@
+//1
+var a = true;
+var b = false;
+console.log =(a === b);
+
+//2
+var num = 2;
+var num2 = 16;
+var moduloResult = 0;
+moduloResult = num % num2;
+console.log(moduloResult);
+
+//3
+let string1 = 'Hej ';
+let string2 = 'ziomek';
+let stringResult = '';
+stringResult = string1 + string2;
+console.log(stringResult);
+
+//4
+var someNumber = 425;
+var someString = "425";
+console.log(someNumber == someString);
+console.log(someNumber === someString);
+// == równe sobie (tutaj true), === równe sobie i mające ten sam typ (tutaj false)
+
+//5
+let counter = 30;
+console.log(counter);
+++counter;
+console.log(counter);
+--counter;
+console.log(counter);
+
+//6
+var a = 30;
+var b = 60;
+var result = null;
+result = a > b;
+console.log(result);
