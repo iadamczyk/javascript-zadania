d6b4e25277d5d0302fe75beb740545f20a14e08a
iadamczyk <78596575+iadamczyk@users.noreply.github.com>
2021-02-05 12:54:14 +0100

    Update script.js

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,19 @@
+//1
+var number = 1;
+var string = 'jeden';
+var numberSt = number + string;
+var logicV = 10 < 5;
+var specialV = null;
+console.log(number, string, numberSt, logicV, specialV);
+
+//2
+var numberOne = 10;
+var numberTwo = 5;
+var sum = 0;
+sum = numberOne + numberTwo;
+console.log(sum);
+
+//3
+var sth;
+console.log(sth);
+//undefined - ponieważ nie ma przypisanej wartości
