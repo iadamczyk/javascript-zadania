a647940d7eef34681c29ef83ac995ce4c3d376a6
iadamczyk <78596575+iadamczyk@users.noreply.github.com>
2021-02-05 12:58:35 +0100

    Update script.js

--- 6.Funkcje/script.js
+++ 6.Funkcje/script.js
@@ -4,21 +4,21 @@ function myFunc () {
   }
   myFunc();
   
-  //2
-  var ab = 22;
-  function myFun(a) {
-    console.log(a);
-  }
-  myFun(ab);
+//2
+var ab = 22;
+function myFun(a) {
+  console.log(a);
+}
+myFun(ab);
   
-  //3
-  var tab1 = [1, 2, 3, 4];
-  function table(tab) {
-    return tab;
-  }
-  console.log(table(tab1));
+//3
+var tab1 = [1, 2, 3, 4];
+function table(tab) {
+  return tab;
+}
+console.log(table(tab1));
 
-  //4
+//4
 function printString(str) {
     let i = 0;
     const timer = setInterval(() => {
